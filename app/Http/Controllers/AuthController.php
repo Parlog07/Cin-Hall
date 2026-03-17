512cbd670a9778b7e1f1c93f99d6a4dc2657537e
krrsalmane <karroumsalmane@gmail.com>
2026-03-17 14:31:17 +0000

    create midelware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
-    //
+   
+
+  
+
+
+    
+
+    
 }
