05ad119f614d2a2a9a567818f542c28adc325254
Parlog07 <ayoubmogador2014@gmail.com>
2026-03-18 13:49:06 +0000

    Add Controller For Film

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FilmController;
 
 Route::get('/', function (Request $request) {
     return response()->json('hellow world');
 });
+
+Route::apiResource('films', FilmController::class);
