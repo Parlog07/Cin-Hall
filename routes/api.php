218b702372e6c9bbcd4e6252854d899a73fe1cbf
Parlog07 <ayoubmogador2014@gmail.com>
2026-03-18 14:18:32 +0000

    update

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FilmController;
-
 Route::get('/', function (Request $request) {
     return response()->json('hellow world');
 });
 
 Route::apiResource('films', FilmController::class);
+Route::get('films', [FilmController::class, 'index']);
+Route::get('films/{film}', [FilmController::class, 'show']);
+Route::post('films', [FilmController::class, 'store']);
+Route::put('films/{film}', [FilmController::class, 'update']);
+Route::delete('films/{film}', [FilmController::class, 'destroy']);
