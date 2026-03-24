34e58b4eef17480281700c414e0f6190f658a817
lahrach el mehdi <lahrachelmehdi2025@gmail.com>
2026-03-24 10:27:02 +0100

    test GitGuardian

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     protected $fillable = ['path' , 'imageable_id' , 'imageable_type'];
-}
+
+    // test for GitGuardian
+
+    }
